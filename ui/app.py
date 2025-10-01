091c03f4f34bae9b7c9668580b5d2b49e0fb98d3
Rawan Essam <167918306+rawanessaam@users.noreply.github.com>
2025-10-01 23:35:58 +0300

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -106,7 +106,7 @@ import plotly.express as px
 # ---------------------------
 # Load model
 # ---------------------------
-model = joblib.load("/models/Best_Model.pkl")
+model = joblib.load("models/Best_Model.pkl")
 
 # ---------------------------
 # Streamlit page setup
@@ -173,7 +173,7 @@ st.markdown("---")
 st.header("📊 Heart Disease Data Insights")
 
 try:
-    df = pd.read_csv("/data/heart.csv")
+    df = pd.read_csv("data/heart.csv")
 
     # 1. Target distribution
     st.subheader("Target Distribution")
