b24e546fa40079eb6d991597924a64606f684883
Rawan Essam <167918306+rawanessaam@users.noreply.github.com>
2025-10-01 23:26:04 +0300

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1J4jEHxuC91zSOlEJ1qXOT6ZcquTbaBno
 """
 
-!pip install streamlit
-
 # import streamlit as st
 # import pandas as pd
 # import joblib
